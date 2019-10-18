d2501507fd7b0a775516c71569a8d6fe4f293605
Emily Fooe <eafooe@users.noreply.github.com>
2019-10-18 13:58:57 -0700

    Play Fur Elise instead of boring tone

--- Code/ConnectToRobot.m
+++ Code/ConnectToRobot.m
@@ -3,8 +3,16 @@
 % Attempt to connect to the robot
 brick = ConnectBrick('EV4');
 
-% Play tone with a frequency of 800Hz for 0.500 seconds at 100% volume
-brick.playTone(100, 800, 500);
+% Test our tones
+brick.playTone(100, 659, 400); %E5
+brick.playTone(100, 622, 400); %D#5
+brick.playTone(100, 659, 400); %E5
+brick.playTone(100, 622, 400); %D#5
+brick.playTone(100, 659, 400); %E5
+brick.playTone(100, 494, 400); %B4
+brick.playTone(100, 587, 400); %D5
+brick.playTone(100, 523, 400); %C5
+brick.playTone(100, 440, 400); %A4
 
 % Display the current battery level
 display(brick.GetBattLevel());
